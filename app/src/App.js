4ba1006f311bf3643fdc96418de07abd6dec32bd
park young jae <jolie0215@gmail.com>
2023-04-15 09:00:56 +0900

    ,

--- app/src/App.js
+++ app/src/App.js
@@ -9,12 +9,11 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/product/*" element={<Chat />}></Route>
+          <Route path="/chat/*" element={<Chat />}></Route>
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
       </BrowserRouter>
-      // asdfsafdasd
     </div>
   );
 }
